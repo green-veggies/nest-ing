44e8402a430683a080ce309bb7dd4422aa6c37b6
Aditya Jasrotia <b322070@iiit-bh.ac.in>
2024-05-25 23:43:40 +0530

    sent response..........received it?

--- src/users.controllers.ts
+++ src/users.controllers.ts
@@ -1,18 +1,59 @@
-import { Controller,Get,Req } from "@nestjs/common";
-import { Request } from "express";
+import {
+  Controller,
+  Get,
+  Post,
+  Req,
+  HttpCode,
+  HttpStatus,
+  Res,
+  Header,
+  Redirect,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 
-@Controller("/users")
+@Controller('/users')
+export class UserController {
+  // return "Hello World from GetUser";
+  @Header('Cache-Control', 'none')
+  @Header('X-Name', 'Aditya')
+  @Get('/profiles')
+  beta(@Req() req: Request) {
+    // console.log(req.body);
 
-export class UserController{
-    // return "Hello World from GetUser";
+    return {
+      name: 'Aditya',
+      message: 'Hello World from getProfiles',
+    };
+  }
 
-    @Get("/profiles")
-    getProfiles(@Req() req:Request) {
-        // console.log(req.body);
-        
-        return {
-            name : "Aditya",
-            message : "Hello World",
-        };
+  // @Redirect("/users/account",302)
+  // @HttpCode(HttpStatus.NO_CONTENT)
+  @Post('/create')
+  @Redirect()
+  alpha(@Req() req: Request) {
+    const randomNum = Math.random() * 10 + 1;
+    if (randomNum <= 5) {
+      return {
+        url: '/users/account',
+        statusCode: 302,
+      };
+    } else {
+      return {
+        url: '/users/wallet',
+        statusCode: 302,
+      };
     }
+    // res.status(201) // it is over-riding HttpStatus.NO_CONTENT
+    // return{
+    //     print:"Hello World from createNew"
+    // };
+  }
+  @Get('/account')
+  userAccount() {
+    return '<h1>This is redirected path to accounts</h1>';
+  }
+  @Get('/wallet')
+  userWallet() {
+    return '<h1>This is redirected path to wallets</h1>';
+  }
 }
