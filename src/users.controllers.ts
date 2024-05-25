933b856bb24ea1bc5a8f1c4903c70ec8112f84f2
Aditya Jasrotia <b322070@iiit-bh.ac.in>
2024-05-25 20:46:18 +0530

    basic routing (brrrrrrt)

--- src/users.controllers.ts
+++ src/users.controllers.ts
@@ -1,12 +1,18 @@
-import { Controller,Get } from "@nestjs/common";
+import { Controller,Get,Req } from "@nestjs/common";
+import { Request } from "express";
 
 @Controller("/users")
 
 export class UserController{
+    // return "Hello World from GetUser";
 
     @Get("/profiles")
-    getProfiles() {
-
+    getProfiles(@Req() req:Request) {
+        console.log(req.body);
+        
+        return {
+            name : "Aditya",
+            message : "Hello World",
+        };
     }
-
 }
